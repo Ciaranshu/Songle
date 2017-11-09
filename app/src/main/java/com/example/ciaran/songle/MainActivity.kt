0f84765602db31f310cde282ab21c2c055b87a68
ciaran <cscshu@outlook.com>
2017-11-09 01:04:33 +0000

    Update Maps

---
+++ app/src/main/java/com/example/ciaran/songle/MainActivity.kt
@@ -0,0 +1,220 @@
+package com.example.ciaran.songle
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import java.io.File
+import java.io.FileInputStream
+
+
+class MainActivity : AppCompatActivity(),
+        NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener{
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var mGoogleApiClient: GoogleApiClient
+    val PERMISSIONS_REQUESR_ACESS_FINE_LOCATION = 1
+    var mLocationPermissionGranted = false 
+    private lateinit var mLastLocation : Location
+    val TAG = "MapsActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build()
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mGoogleApiClient.connect()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mGoogleApiClient.isConnected) {
+            mGoogleApiClient.disconnect()
+        }
+    }
+
+    fun createLocationRequest() {
+
+        // Set the parameters for the location request
+        val mLocationRequest = LocationRequest()
+            mLocationRequest.interval = 5000 // preferably every 5 seconds
+            mLocationRequest.fastestInterval = 1000 // at most every second
+            mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        // Can we access the user’s current location?
+
+        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this)
+        }
+
+    }
+
+    override fun onConnected(connectionHint : Bundle?) {
+
+        try { createLocationRequest(); } catch (ise : IllegalStateException) {
+
+            println("IllegalStateException thrown [onConnected]"); } // Can we access the user’s current location?
+
+        if (ContextCompat.checkSelfPermission(this,
+
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUESR_ACESS_FINE_LOCATION); }
+
+    }
+
+    override fun onLocationChanged(current : Location?) {
+
+        if (current == null) {
+            println("[onLocationChanged] Location unknown")
+        } else {
+            println("""[onLocationChanged] Lat/long now
+
+            (${current.getLatitude()},
+
+            ${current.getLongitude()})"""
+
+            ) }
+    }
+
+    override fun onConnectionSuspended(ﬂag : Int) {
+        println(" >>>> onConnectionSuspended")
+    }
+
+    override fun onConnectionFailed(result : ConnectionResult) {
+        println(" >>>> onConnectionFailed")
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Move and zoom the camera
+        val centre = LatLng(55.944424999999995, -3.188396)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centre, 17F))
+
+        try {
+            // Visualise current position with a small blue circle
+            mMap.isMyLocationEnabled = true
+
+            // Add "My location" button to the user interface
+            mMap.uiSettings.isMyLocationButtonEnabled = true
+
+            // Add [+] and [-] zoom controls
+            mMap.uiSettings.isZoomControlsEnabled = true
+
+        } catch (se : SecurityException) {
+            println(">>>>> [$TAG] Security exception thrown [onMapReady]")
+        }
+
+
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
+
+
